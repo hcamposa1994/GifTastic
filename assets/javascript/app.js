d3ac5eee08de75f80d3a0858d8f85550d8732d45
Herbert Campos <wohoforever@yahoo.com>
2019-05-17 19:25:18 -0700

    finished the ajex portion, and gifs are sucessfully printing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,7 @@ function renderButtons() {
       var newfruit = $("<button>");
       newfruit.text(topics[i]);
       newfruit.attr("data-fruit", topics[i]);
+      newfruit.attr("class", "fruit-group");
       $("#fruit-buttons").append(newfruit);
 
     }
@@ -27,3 +28,42 @@ $("#add-fruit").on("click", function() {
   });
 
 renderButtons();
+
+$(document).on("click", ".fruit-group", function() {
+    var fruit = $(this).attr("data-fruit");
+    var api_key = "&api_key=GFIHFicY1XEDTegb3grxyAzfTQ35fowX&limit=10";
+    var limit = "&limit=10"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      fruit + api_key + limit;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      console.log(response);
+
+        var results = response.data;
+        $("#fruit-gifs").empty();
+       for (var i = 0; i < results.length; i++) {
+           
+      var fruitDiv = $("<div>");
+
+      var p = $("<p>");
+
+      p.text(results[i].rating);
+
+      var fruitGif = $("<img>");
+
+      fruitGif.attr('src', results[i].images.fixed_height.url);
+
+      fruitDiv.append(p);
+
+      fruitDiv.append(fruitGif);
+
+      $("#fruit-gifs").prepend(fruitDiv);
+
+       }
+
+    });
+  });
